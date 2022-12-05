f588a0febb4c08a3fcbdfd43fc290ca5e46af4f2
teleginzhenya <telegin.zhenya@gmail.com>
2022-12-05 12:01:13 +0600

    Day 5 solutions

---
+++ src/day5_b.ts
@@ -0,0 +1,69 @@
+import { input } from "./day5_input";
+
+const rows = input.split('\n');
+
+let idx = 0;
+let currentRow = rows[idx];
+
+const hashTable: Record<string, string[]> = {};
+
+while (!Number.isInteger(parseInt(currentRow[1], 10))) {
+    const splittedRow = currentRow.match(/.{1,4}/g);
+
+    splittedRow!.map((item) => item.replaceAll(' ', '')).forEach((item, idx) => {
+        if (item === '') {
+            return;
+        }
+
+        const sanitizedItem = item.replaceAll('[', '').replaceAll(']', '');
+
+        if (hashTable[idx] && hashTable[idx].length > 0) {
+            hashTable[idx].push(sanitizedItem);
+        } else {
+            hashTable[idx] = [sanitizedItem];
+        }
+    })
+
+    idx += 1;
+    currentRow = rows[idx];
+}
+
+Object.entries(hashTable).forEach(([key, value]) => {
+    hashTable[key] = value.reverse();
+})
+
+type CommandType = Record<'amount' | 'from' | 'to', number>;
+
+const commands: CommandType[] = [];
+
+idx += 2;
+currentRow = rows[idx];
+
+while (currentRow && currentRow.length > 0) {
+    const [amount, from, to] = currentRow.replaceAll('move ', '').replaceAll('from ', '').replaceAll('to ', '').split(' ').map((item) => parseInt(item, 10));
+    commands.push({ amount, from, to })
+    idx += 1;
+    currentRow = rows[idx];
+}
+
+commands.forEach(({ amount, from, to }, index) => {
+    let i = 0;
+    const popedArray = [];
+    while (i < amount) {
+        popedArray.push(hashTable[from - 1].pop()!);
+        i += 1;
+    }
+    hashTable[to - 1].push(...popedArray.reverse());
+})
+
+const answer = Object.values(hashTable).reduce<string>((acc, item) => {
+    if (item.length > 0) {
+        acc += item.pop();
+    }
+
+    return acc;
+}, '')
+
+console.log(answer);
+
+export { };
