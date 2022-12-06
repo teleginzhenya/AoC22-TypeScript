9fc54a16d07317d5fce17cb31c7f6f9c1fa234f8
teleginzhenya <telegin.zhenya@gmail.com>
2022-12-06 11:20:55 +0600

    Day 6 solutions

---
+++ src/day6_b.ts
@@ -0,0 +1,28 @@
+import { input } from "./day6_input";
+
+const items = input.split('');
+
+let i = 0;
+let answer = -1;
+const n = 14;
+
+while ((i < items.length - n) && answer < 0) {
+    const set = new Set();
+    let result = true;
+
+    for (let j = i; j < i + n; j += 1) {
+        if (set.has(items[j])) {
+            set.add(items[j]);
+            result = false;
+        } else {
+            set.add(items[j]);
+        }
+    }
+
+    i += 1;
+    if (result) {
+        answer = i + n - 1;
+    }
+}
+
+console.log(answer);
